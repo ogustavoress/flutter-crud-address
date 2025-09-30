dd336c65b8c33e87bdc60ce1e3a6373e45605b22
Gustavo <ogustavoress@gmail.com>
2025-09-30 16:46:48 -0300

    feat(homepage): adicionando initState e fetchAdresses para carregar a lista da API

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
+import 'package:crud_address_app/api_config.dart';
 import 'package:crud_address_app/model/address.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,8 +16,24 @@ class _HomePageState extends State<HomePage>{
   List<Address> address = [];
 
   @override
+  void initState() {
+    super.initState();
+    fetchAddresses();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Container();
+  }
+
+  Future<void> fetchAddresses() async {
+    final response = await http.get(Uri.parse(ApiConfig.addresses()));
+    if (response.statusCode == 200) {
+      final List<dynamic> data = json.decode(response.body);
+      setState(() {
+        address = data.map((e) => Address.fromJson(e)).toList();
+      });
+    }
   }
 }
 
