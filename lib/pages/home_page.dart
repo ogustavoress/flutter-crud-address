38a4783da4d354a18cf785a123988fe012d170ea
Gustavo <ogustavoress@gmail.com>
2025-09-30 16:17:30 -0300

    feat(homepage): criando a estrutura inicial da página

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:crud_address_app/model/address.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>{
+  List<Address> address = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+
