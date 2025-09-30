6a0b28fca8ec6799a338adba92fe0ba1cad419da
Gustavo <ogustavoress@gmail.com>
2025-09-30 16:51:12 -0300

    feat(homepage): criando o build e método que abre uma nova tela/formulário

--- lib/model/address.dart
+++ lib/model/address.dart
@@ -4,8 +4,8 @@ class Address {
   final String cep;
   final String logradouro;
   final String bairro;
+  final String cidade;
   final String uf;
-  final String tipo;
 
   Address({
     required this.id,
@@ -13,8 +13,8 @@ class Address {
     required this.cep,
     required this.logradouro,
     required this.bairro,
+    required this.cidade,
     required this.uf,
-    required this.tipo
   });
 
   factory Address.fromJson(Map<String, dynamic> json) {
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -13,7 +13,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage>{
-  List<Address> address = [];
+  List<Address> addresses = [];
 
   @override
   void initState() {
@@ -23,7 +23,30 @@ class _HomePageState extends State<HomePage>{
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Endereços"),
+      ),
+      body: ListView.builder(
+        itemCount: addresses.length,
+        itemBuilder: (context, index) {
+          final addr = addresses[index];
+          return ListTile(
+            title: Text(addr.nomeUsuario),
+            subtitle: Text(
+                "${addr.logradouro}, ${addr.bairro}, "
+                "${addr.cidade}/${addr.uf},"
+            ),
+            trailing: const Icon(Icons.arrow_forward_ios),
+            onTap: () => goToForm(address: addr),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => goToForm(),
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 
   Future<void> fetchAddresses() async {
@@ -31,9 +54,17 @@ class _HomePageState extends State<HomePage>{
     if (response.statusCode == 200) {
       final List<dynamic> data = json.decode(response.body);
       setState(() {
-        address = data.map((e) => Address.fromJson(e)).toList();
+        addresses = data.map((e) => Address.fromJson(e)).toList();
       });
     }
+  }
+
+  void goToForm({Address? address}) async {
+    await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (_) => AddressFormPage(address: address)),
+    );
+    fetchAddresses();
   }
 }
 
