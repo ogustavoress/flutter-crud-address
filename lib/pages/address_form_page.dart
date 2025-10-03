edc833555a6b38d0d11ca7443efbe5a403452518
Gustavo <ogustavoress@gmail.com>
2025-10-03 00:19:47 -0300

    feat(address_form): criando a estrutura da página de formulário

---
+++ lib/pages/address_form_page.dart
@@ -0,0 +1,20 @@
+import 'package:crud_address_app/model/address.dart';
+import 'package:flutter/material.dart';
+
+class AddressFormPage extends StatefulWidget {
+  final Address? address;
+
+  const AddressFormPage({super.key, this.address});
+
+  @override
+  State<AddressFormPage> createState() =>
+    _AddressFormPageState();
+
+}
+
+class _AddressFormPageState extends State<AddressFormPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
